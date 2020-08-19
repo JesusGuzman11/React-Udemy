3b3596e3ccdb782a224ae4c5a436730fd4f21833
Jesus Guzman <jesusguzman.0311@gmail.com>
2020-08-19 14:32:04 -0500

    Start React Course

---
+++ repasojs/variables/crearVariables/js/app.js
@@ -0,0 +1,23 @@
+/* ----------------------------- Crear Variables ---------------------------- */
+/* ----------------------------------- Var ---------------------------------- */
+var aprendiendo = true;
+aprendiendo = false;
+aprendiendo = 8;
+aprendiendo = 'Jesus';
+console.log(aprendiendo)
+/* -------------------------------------------------------------------------- */
+var aprendiendo2;
+aprendiendo2 = 'Jesus2'
+console.log(aprendiendo2);
+/* -------------------------------------------------------------------------- */
+/* ---------------------------------- Const --------------------------------- */
+// const aprendiendoConst => Error
+const aprendiendoConst = 'JavaScript';
+// aprendiendoConst = false; => Error
+console.log(aprendiendoConst);
+/* -------------------------------------------------------------------------- */
+/* ----------------------------------- Let ---------------------------------- */
+let aprendiendoLet = 'JavaScript2';
+// let aprendiendoLet;
+aprendiendoLet = false;
+console.log(aprendiendoLet);
